9866ec5a105e316bb152c77c144a1b9b39150772
Kaloyan <i500862@fhict.nl>
2024-03-07 15:52:40 +0100

    Started implementing the REST APIs

--- src/main/java/s3/ind/controller/UsersController.java
+++ src/main/java/s3/ind/controller/UsersController.java
@@ -1,12 +1,58 @@
 package s3.ind.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import s3.ind.domain.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
 public class UsersController {
+    private List<User> users = createMockUsers();
+
+    private List<User> createMockUsers() {
+        List<User> mockUsers = new ArrayList<>();
+
+        User user1 = User.builder()
+                .id(1L)
+                .firstName("John")
+                .lastName("Doe")
+                .country("USA")
+                .build();
+
+        User user2 = User.builder()
+                .id(2L)
+                .firstName("Rali")
+                .lastName("Stoyanova")
+                .country("Burgas")
+                .build();
+
+        mockUsers.add(user1);
+        mockUsers.add(user2);
+
+        return mockUsers;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        // List<User> response = new ArrayList<>(users);
+        List<User> response = createMockUsers();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUser(@PathVariable(value = "id") final long id) {
+        final Optional<User> userOptional = users.stream().filter(user -> user.getId() == id).findFirst();
+
+        return userOptional.map(user -> ResponseEntity.ok().body(user)).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
 
 }
---
+++ src/main/java/s3/ind/domain/User.java
@@ -0,0 +1,18 @@
+package s3.ind.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String country;
+}
