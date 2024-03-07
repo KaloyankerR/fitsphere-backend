fc46c01bad47fdedb0f68c19c0a5501bf7d82f53
Kaloyan <i500862@fhict.nl>
2024-03-07 15:18:49 +0100

    Initialization of the user controller

---
+++ src/main/java/s3/ind/controller/UsersController.java
@@ -0,0 +1,12 @@
+package s3.ind.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@AllArgsConstructor
+public class UsersController {
+
+}
