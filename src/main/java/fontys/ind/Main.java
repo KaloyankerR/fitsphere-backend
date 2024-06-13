91c45003c9fcd34e629c0fb7c1af023df4fe377c
Kaloyan <i500862@fhict.nl>
2024-06-13 20:24:24 +0200

    change port settings

--- src/main/java/fontys/ind/Main.java
+++ src/main/java/fontys/ind/Main.java
@@ -1,13 +1,27 @@
 package fontys.ind;
 
+import org.hibernate.cfg.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 // @SpringBootApplication() TODO: check this
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> {
+            String port = "8080";
+            if (port != null) {
+                factory.setPort(Integer.parseInt(port));
+            }
+        };
     }
 }
